5cb91746ceebc0de99c18a8086b2063436859bfe
Arie k. <67206811+a972k@users.noreply.github.com>
2025-07-23 17:02:31 +0300

    Update move_s3_files.py
    added comments

--- move_s3_files.py
+++ move_s3_files.py
@@ -1,9 +1,10 @@
 import boto3
 import os
-from resources import source_buket, destination_bucket, prefix, destination_folder, sns_topic_arn
+from resources import source_bucket, destination_bucket, prefix, destination_folder, sns_topic_arn
+
 
 # Update these values
-source_buket = 'my-source-bucket-ak'
+source_bucket = 'my-source-bucket-ak'
 destination_bucket = 'my-destination-bucket-ak'
 prefix = 'customer-details/sr1_'
 destination_folder = 'sr1/'
@@ -13,24 +14,25 @@ sns_topic_arn = 'arn:aws:sns:us-east-1:123456789012:file-move-notification'
 s3 = boto3.client('s3')
 sns = boto3.client('sns')
 
+# Function to move files from source to destination bucket(can be defined as the same bucket)
 def move_files():
-    response = s3.list_objects_v2(Bucket= source_buket, Prefix=prefix)
+    response = s3.list_objects_v2(Bucket= source_bucket, Prefix=prefix)
     files_moved = []
 
     for obj in response.get('Contents', []):
         key = obj['Key']
         filename = os.path.basename(key)
-        dest_key = f'{destination_bucket}{filename}'
+        dest_key = f'{destination_folder}{filename}'
 
         # Copy file
         s3.copy_object(
             Bucket=destination_bucket,
-            CopySource={'Bucket': source_buket, 'Key': key},
+            CopySource={'Bucket': source_bucket, 'Key': key},
             Key=dest_key
         )
 
         # Delete source file
-        s3.delete_object(Bucket=source_buket, Key=key)
+        s3.delete_object(Bucket=source_bucket, Key=key)
 
         print(f"Moved: {key} ➜ {dest_key}")
         files_moved.append(filename)
